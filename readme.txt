59b675abbbd9623d624979bb4f5ea6f1f836f5c3
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:26:24 -0400

    First commit

---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: LAMP 17 - Associative Arrays
+Published: 11/3/2012 1:59:11 PM
+Link: https://www.youtube.com/watch?v=gmpAbIzRXQ0
+
+Description: Make arrays easier to work with use Associative Arrays
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
---
+++ tut17.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+
+<body>
+
+<?php
+
+$colors = array("Bryan"=>"blue", "Heather"=>"black", "Florence"=>"pink");
+
+
+echo $colors['Heather'] . "<br>";
+
+echo "Florence's fav color is: " . $colors['Florence'] . "<br>";
+
+?>
+
+</body>
+</html>
